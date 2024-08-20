0525028c58fffb95b5b47ca0cfa7be0dbd209338
Lev Kadatskyi <levkadatscky@gmail.com>
2024-08-20 13:53:04 +0300

    added functions for detecting edges and contours

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import cv2
+import imutils
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -90,3 +92,47 @@ def four_point_transform(image, pts):
     warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
 
     return warped
+
+
+def detect_edges(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 75, 200)
+
+    return edged
+
+
+def detect_contours(edged_image):
+    cnts = cv2.findContours(edged_image.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+
+    for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+        if len(approx) == 4:
+            screenCnt = approx
+            break
+
+    return screenCnt
+
+
+image = cv2.imread("test_image.jpg")
+ratio = image.shape[0] / 500.0
+orig = image.copy()
+
+image = imutils.resize(image, height=500)
+edged = detect_edges(image)
+contours = detect_contours(edged)
+image_contours = cv2.polylines(
+    image.copy(), [contours], isClosed=True, color=(0, 255, 0), thickness=2
+)
+
+plt.subplot(131), plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.title("Original Image"), plt.xticks([]), plt.yticks([])
+plt.subplot(132), plt.imshow(cv2.cvtColor(edged, cv2.COLOR_BGR2RGB))
+plt.title("Edged Image"), plt.xticks([]), plt.yticks([])
+plt.subplot(133), plt.imshow(cv2.cvtColor(image_contours, cv2.COLOR_BGR2RGB))
+plt.title("Image with Contours"), plt.xticks([]), plt.yticks([])
+plt.show()
